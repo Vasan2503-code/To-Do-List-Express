f5d1b4b2508dd1cce4bd78c2ab91baac7a766a7f
Vasan2503-code <srinivasansiet142@gmail.com>
2026-01-27 11:57:26 +0530

    frontend deploy

--- client/src/api/Task.js
+++ client/src/api/Task.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "http://localhost:3000/api",
+    baseURL : "https://mern-to-do-list-t1is.onrender.com/api",
     header : {
         "Content-Type" : "application/json"
     }
